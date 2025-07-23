a82f11773696fa8936a59ba49511035a791051fb
Dharneesh-009 <dharaneeshprabhakaran@gmail.com>
2025-07-23 19:40:03 +0530

    connection

--- src/main/java/com/virutualtask/kanban_backend/config/WebConfig.java
+++ src/main/java/com/virutualtask/kanban_backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // ✅ React app origin
+                .allowedOrigins("https://kanban-frontend-9e5h.onrender.com") // ✅ React app origin
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowCredentials(true);
